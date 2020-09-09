7402d4b2571f885d2576282d51af3e8681071ed0
Thilagavathy <thilagarajesh98@gmail.com>
2020-09-09 16:42:54 +0800

    private key and public key generation functionality added

--- blockchain_client.py
+++ blockchain_client.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
+import Crypto
+import Crypto.Random
+from Crypto.PublicKey import RSA
+import binascii
+
 
 class Transactions:
     def __init__(self, sender_address, sender_private_key, recipient_address, value):
@@ -24,3 +29,19 @@ def make_transactions():
 @app.route('/view/transactions')
 def view_transactions():
     return render_template("view_transactions.html")
+
+
+@app.route('/wallet/new')
+def new_wallet():
+    random_gen = Crypto.Random.new().read
+    private_key = RSA.generate(1024, random_gen)
+    public_key = private_key.publickey()
+    response = {
+        'private_key': binascii.hexlify(private_key.export_key(format('DER'))).decode('ascii'),
+        'public_key': binascii.hexlify(public_key.export_key(format('DER'))).decode('ascii')
+    }
+    return jsonify(response), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
